3d31cbdcd8fdf6cba5a94d7bc689d86cb984bace
Sourasish01 <bhotto2023@gmail.com>
2025-02-18 02:40:51 +0530

    hardhat: ignition_setup, hardhat_config

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.PROVIDER_URL,
+      accounts: process.env.PRIVATE_KEY,
+    },
+  },
 };
